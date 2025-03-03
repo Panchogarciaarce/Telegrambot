81eeeba8b1951f526c44c0f18baa18742e8905b9
Panchogarciaarce <jfgarciaarce@gmail.com>
2025-03-02 23:49:38 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Obtener el TOKEN desde las variables de entorno
-TOKEN = os.getenv("8068879567:AAFFh_CiTpHnJ2ahU_A5E34601W-KcEgToQ")
+# Obtener el TOKEN desde Railway
+TOKEN = os.getenv("TOKEN")
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("¡Hola! Soy un bot de Telegram funcionando en Railway.")
